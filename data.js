7770940a26443e47acffaa95c2bd4839f2f84fb8
Sreerag <sreeragpvsreeragam063@gmail.com>
2025-08-30 13:33:22 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,325 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-living-area",
+      "name": "Living Area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.109222398474275,
+          "pitch": 0.15220636614624183,
+          "rotation": 0,
+          "target": "2-pari-bedroom"
+        },
+        {
+          "yaw": -1.9161583053002254,
+          "pitch": 0.3642799717463401,
+          "rotation": 0,
+          "target": "1-karan-bedroom"
+        },
+        {
+          "yaw": 2.5938365025816736,
+          "pitch": 0.1781119030626641,
+          "rotation": 0,
+          "target": "6-living-02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-karan-bedroom",
+      "name": "Karan Bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9593956881617203,
+          "pitch": 0.29686897220300246,
+          "rotation": 0,
+          "target": "0-living-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-pari-bedroom",
+      "name": "Pari Bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7535526934377446,
+          "pitch": 0.25521408594886275,
+          "rotation": 0,
+          "target": "0-living-area"
+        },
+        {
+          "yaw": 1.6759646674873103,
+          "pitch": 0.0667013986352103,
+          "rotation": 0,
+          "target": "1-karan-bedroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-arjun-bedroom",
+      "name": "Arjun Bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1704107303170463,
+          "pitch": 0.28477113583753955,
+          "rotation": 0,
+          "target": "6-living-02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-guest-bedroom",
+      "name": "Guest Bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.150887432506085,
+          "pitch": 0.1712665969462961,
+          "rotation": 0,
+          "target": "6-living-02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-kitchen",
+      "name": "Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.010842754036922,
+          "pitch": 0.3889289901394122,
+          "rotation": 0,
+          "target": "6-living-02"
+        },
+        {
+          "yaw": 2.830308068064717,
+          "pitch": 0.1278178760535198,
+          "rotation": 0,
+          "target": "4-guest-bedroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-living-02",
+      "name": "Living 02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.842762103735545,
+          "pitch": 0.19353458057774553,
+          "rotation": 0,
+          "target": "0-living-area"
+        },
+        {
+          "yaw": -0.5246151993446162,
+          "pitch": 0.18086593135811846,
+          "rotation": 0.7853981633974483,
+          "target": "2-pari-bedroom"
+        },
+        {
+          "yaw": 0.6696326968464383,
+          "pitch": 0.32555320462201465,
+          "rotation": 0,
+          "target": "5-kitchen"
+        },
+        {
+          "yaw": 1.6945104978598096,
+          "pitch": 0.49501147370465226,
+          "rotation": 0,
+          "target": "3-arjun-bedroom"
+        },
+        {
+          "yaw": -2.554328759127376,
+          "pitch": 0.19942538729549497,
+          "rotation": 0,
+          "target": "4-guest-bedroom"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
